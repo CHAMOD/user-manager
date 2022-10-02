cd2628cfb1c1562e58a3ebb43cb9865aeaf1e248
DESKTOP-C2DGVES\Chamod <chamod@productfy.io>
2022-10-02 14:59:09 +0530

    API error handler

--- src/main/java/com/user/api/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/user/api/exceptions/ApiExceptionHandler.java
@@ -1,41 +1,27 @@
 
 package com.user.api.exceptions;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
-import java.util.LinkedHashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-//  @ExceptionHandler(MethodArgumentNotValidException.class)
-//  @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-//  @ResponseBody
-//  public ApiError handleValidationError(MethodArgumentNotValidException ex) {
-//    BindingResult bindingResult = ex.getBindingResult();
-//    FieldError fieldError = bindingResult.getFieldError();
-//    String defaultMessage = fieldError.getDefaultMessage();
-//    return new ApiError("VALIDATION_FAILED", defaultMessage);
-//  }
-
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(
     MethodArgumentNotValidException ex, HttpHeaders headers,
     HttpStatus status, WebRequest request) {
-
-    Map<String, Object> body = new LinkedHashMap<>();
-    body.put("timestamp", LocalDate.now());
-    body.put("status", status.value());
 
     List<String> errors = ex.getBindingResult()
       .getFieldErrors()
@@ -43,7 +29,40 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
       .map(x -> x.getDefaultMessage())
       .collect(Collectors.toList());
 
-    body.put("errors", errors);
-    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    ErrorMessage message = new ErrorMessage(
+      HttpStatus.BAD_REQUEST.value(),
+      new Date(),
+      errors.toString(),
+      request.getDescription(false));
+
+    return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
   }
+
+  @Override
+  protected ResponseEntity<Object> handleTypeMismatch(final TypeMismatchException ex,
+                                                      final HttpHeaders headers,
+                                                      final HttpStatus status,
+                                                      final WebRequest request) {
+
+
+    ErrorMessage message = new ErrorMessage(
+      HttpStatus.BAD_REQUEST.value(),
+      new Date(),
+      ex.getMessage(),
+      request.getDescription(false));
+
+    return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+      HttpStatus.INTERNAL_SERVER_ERROR.value(),
+      new Date(),
+      ex.getMessage(),
+      request.getDescription(false));
+
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
 }
